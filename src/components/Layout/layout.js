c97a316320f0eeb1b729791db55ba8c01dc1b1e8
UnsignedArduino <UnsignedArduino@outlook.com>
2022-08-02 14:59:39 -0400

    Part of part 4: Show site title on page

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,11 +1,23 @@
 import * as React from "react";
-import { container, heading } from "./layout.module.css";
+import { container, heading, siteTitle } from "./layout.module.css";
+import { useStaticQuery, graphql } from "gatsby";
 import Navbar from "../Navbar";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <Navbar/>
       </nav>
